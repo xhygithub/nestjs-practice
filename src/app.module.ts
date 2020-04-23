f17b7484805094f39db39c0665a2763e96891b4d
haiyan xie <haiyanxie@haiyans-MacBook-Pro-2.local>
2020-04-23 14:58:37 +0800

    add health check for this application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import {TerminusModule} from '@nestjs/terminus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TerminusModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/health/health.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { DNSHealthIndicator, HealthCheck, HealthCheckService } from '@nestjs/terminus';
+
+@Controller('health')
+export class HealthController {
+  constructor(private dns: DNSHealthIndicator, private health: HealthCheckService,) {}
+
+  @Get()
+  @HealthCheck()
+  healthCheck() {
+    return this.health.check([
+      async () => this.dns.pingCheck('google', 'https://google.com'),
+    ]);
+  }
+}
